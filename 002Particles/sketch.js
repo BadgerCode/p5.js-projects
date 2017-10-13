9cf39a38cefed1ed06ce2d15ba80f831d64eae51
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-10-13 16:47:35 +0100

    Jazzes up particles

--- 002Particles/sketch.js
+++ 002Particles/sketch.js
@@ -1,14 +1,17 @@
 let particles = [];
 let iterationsTilSpawn = 0;
+let hue = 5;
+let hueDir = 5;
 
 function setup() {
   createCanvas(640, 480);
+  colorMode(HSB);
 
   stroke(0, 0);
 }
 
 function draw() {
-    background(255);
+    background(0);
 
     for(var i = particles.length - 1; i >= 0; i--){
         particles[i].draw();
@@ -19,8 +22,12 @@ function draw() {
     }
 
     if(iterationsTilSpawn <= 0){
-        particles.push(new Particle());
-        iterationsTilSpawn = Random(3, 8);
+        particles.push(new Particle(hue));
+        hue += hueDir;
+        if(hue <= 5 || hue >= 45)
+            hueDir *= -1;
+
+        iterationsTilSpawn = Random(6, 13);
     }
     else {
         iterationsTilSpawn--;
@@ -32,24 +39,25 @@ function Random(min, max){
 }
 
 class Particle {
-    constructor(){
+    constructor(hue){
         this.x = 320;
         this.y = 465;
-        this.fill = 255;
+        this.alpha = 255;
+        this.hue = hue;
     }
 
     update(){
-        this.fill -= 2;
+        this.alpha -= 3;
         this.x += Random(-1, 1);
         this.y += Random(-5, -2);
     }
 
     shouldKill(){
-        return this.fill <= 0;
+        return this.alpha <= 0;
     }
 
     draw(){
-        fill(255, 204, 0, this.fill);
-        ellipse(this.x, this.y, 20);
+        fill(this.hue, 255, this.alpha);
+        ellipse(this.x, this.y, 50);
     }
 }
