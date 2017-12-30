7608802644d50bb691246d8d8d731e5e77d976f9
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-12-30 23:02:28 +0000

    80sRetro: Cool purple landscape

--- 0080sRetro/sketch.js
+++ 0080sRetro/sketch.js
@@ -1,16 +1,45 @@
-function setup(){
+let cols = 20;
+let rows = 40;
+let boxWidth = 40;
+let xOffset = -cols * boxWidth / 2;
+let yOffset = -rows * boxWidth / 2;
+let noiseScale = 0.0055;
+let noiseYOffset = 0;
+
+function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
-    colorMode(HSB);
+    colorMode(HSB)
 }
 
-function draw(){
-    background(15);
+function draw() {
+    background(0);
+
+    translate(0, 200, 300);
+    rotateX(PI / 2.5);
+
+    translate(xOffset, yOffset, 0);
 
     noFill();
-    stroke(255);
+    stroke(280, 100, 100);
 
-    push();
-        rotateY(frameCount * 0.1);
-        rect(10, 10, 40, 40);
-    pop();
+    for (var y = 0; y < rows; y++) {
+        for (var x = 0; x < cols; x++) {
+            beginShape(TRIANGLES);
+            drawPoint(x * boxWidth, y * boxWidth);
+            drawPoint(x * boxWidth, (y+1) * boxWidth);
+            drawPoint((x+1) * boxWidth, y * boxWidth);
+            endShape();
+            beginShape(TRIANGLES);
+            drawPoint(x * boxWidth, (y+1) * boxWidth);
+            drawPoint((x+1) * boxWidth, y * boxWidth);
+            drawPoint((x+1) * boxWidth, (y+1) * boxWidth);
+            endShape();
+        }
+    }
+
+    //noiseYOffset -= 30;
+}
+
+function drawPoint(x, y){
+    vertex(x, y, map(noise(x * noiseScale, (y+noiseYOffset) * noiseScale), 0, 1, 0, 200));
 }
