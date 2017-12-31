27445e8c1df468c789045ff1258fe18036e17851
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-12-31 02:11:59 +0000

    80sRetro: Adds FPS count

--- 0080sRetro/sketch.js
+++ 0080sRetro/sketch.js
@@ -2,22 +2,27 @@ let cols = 25;
 let rows = 40;
 let boxWidth = 40;
 let xOffset = -cols * boxWidth / 2;
-let yOffset = -rows * boxWidth + 400;
+let yOffset = -rows * boxWidth + 195;
 let noiseScale = 0.1;
 let noiseYOffset = 0;
 
-let heightMap;
+let heightMap = Array();
 let heightMapOffset = 0;
 let heightMapPosition = 0;
 
 let maxHue = 40;
 let hueMultiplier = maxHue / rows;
+let hues = Array();
+
+let fpsDiv;
+let frameDurationAverage = 0;
+let frameDurationReadings = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
+    fpsDiv = createFPSElement();
     colorMode(HSB)
 
-    heightMap = Array();
     for (var y = 0; y < rows+1; y++) {
         var row = Array();
         for (var x = 0; x < cols+1; x++) {
@@ -25,14 +30,16 @@ function setup() {
         }
 
         heightMap.push(row);
+        hues.push(maxHue - hueMultiplier * y);
     }
 }
 
 function draw() {
+    var startTime = millis();
     background(10);
 
     translate(0, 200, 300);
-    rotateX(PI / 2.5);
+    rotateX(PI / 3.5);
 
     translate(xOffset, yOffset, 0);
 
@@ -40,20 +47,24 @@ function draw() {
     
     for (var y = 0; y < rows; y++) {
         for (var x = 0; x < cols; x++) {
-            stroke(maxHue - hueMultiplier * y, 100, 100);
+            var h1 = getHeight(y+1, x);
+            var h2 = getHeight(y, x+1);
+
+            stroke(hues[y], 100, 100);
             beginShape(TRIANGLES);
             vertex(x * boxWidth, y * boxWidth, getHeight(y, x));
-            vertex(x * boxWidth, (y+1) * boxWidth, getHeight(y+1, x));
-            vertex((x+1) * boxWidth, y * boxWidth, getHeight(y, x+1));
+            vertex(x * boxWidth, (y+1) * boxWidth, h1);
+            vertex((x+1) * boxWidth, y * boxWidth, h2);
 
-            vertex(x * boxWidth, (y+1) * boxWidth, getHeight(y+1, x));
-            vertex((x+1) * boxWidth, y * boxWidth, getHeight(y, x+1));
+            vertex(x * boxWidth, (y+1) * boxWidth, h1);
+            vertex((x+1) * boxWidth, y * boxWidth, h2);
             vertex((x+1) * boxWidth, (y+1) * boxWidth, getHeight(y+1, x+1));
             endShape();
         }
     }
 
     shiftHeightMapRows();
+    addFrameDuration(millis() - startTime);
 }
 
 function getHeight(y, x){
@@ -74,4 +85,21 @@ function shiftHeightMapRows(){
     heightMapOffset--;
     if(heightMapOffset < 0)
         heightMapOffset = rows;
+}
+
+function createFPSElement(){
+    var div = createDiv("Message");
+
+    div.style('position', 'absolute');
+    div.style('color', 'white');
+    div.style('left', '0');
+    div.style('top', '0');
+
+    return div;
+}
+
+function addFrameDuration(frameDuration){
+    frameDurationAverage = (frameDurationAverage * frameDurationReadings++ + frameDuration) / frameDurationReadings;
+
+    fpsDiv.html("FPS: " + Math.round(1000/frameDurationAverage) + "<br>Frame duration: " + Math.round(frameDurationAverage));
 }
