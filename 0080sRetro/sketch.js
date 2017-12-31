0f189c5726b6fc75b5ba6222a59ec707eee74f7c
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-12-31 02:44:01 +0000

    80sRetro: Improves performance

--- 0080sRetro/sketch.js
+++ 0080sRetro/sketch.js
@@ -1,8 +1,8 @@
-let cols = 25;
-let rows = 40;
+let cols = 40;
+let rows = 30;
 let boxWidth = 40;
 let xOffset = -cols * boxWidth / 2;
-let yOffset = -rows * boxWidth + 195;
+let yOffset = -rows * boxWidth * 0.80;
 let noiseScale = 0.1;
 let noiseYOffset = 0;
 
@@ -18,10 +18,14 @@ let fpsDiv;
 let frameDurationAverage = 0;
 let frameDurationReadings = 0;
 
+let lastFrameStart;
+
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
     fpsDiv = createFPSElement();
     colorMode(HSB)
+
+    frameRate(30);
 
     for (var y = 0; y < rows+1; y++) {
         var row = Array();
@@ -32,10 +36,13 @@ function setup() {
         heightMap.push(row);
         hues.push(maxHue - hueMultiplier * y);
     }
+
+    lastFrameStart = millis();
 }
 
 function draw() {
-    var startTime = millis();
+    addFrameDuration(millis() - lastFrameStart);
+    lastFrameStart = millis();
     background(10);
 
     translate(0, 200, 300);
@@ -43,28 +50,30 @@ function draw() {
 
     translate(xOffset, yOffset, 0);
 
-    noFill();
+    noStroke();
     
     for (var y = 0; y < rows; y++) {
         for (var x = 0; x < cols; x++) {
-            var h1 = getHeight(y+1, x);
-            var h2 = getHeight(y, x+1);
+            var heightTL = getHeight(y, x);
+            var heightBL = getHeight(y+1, x);
+            var heightTR = getHeight(y, x+1);
+            var heightBR = getHeight(y+1, x+1);
 
-            stroke(hues[y], 100, 100);
-            beginShape(TRIANGLES);
-            vertex(x * boxWidth, y * boxWidth, getHeight(y, x));
-            vertex(x * boxWidth, (y+1) * boxWidth, h1);
-            vertex((x+1) * boxWidth, y * boxWidth, h2);
+            fill(hues[y], 100, 100);
+            beginShape(LINES);
+            vertex(x * boxWidth, y * boxWidth, heightTL);
+            vertex(x * boxWidth, (y+1) * boxWidth, heightBL);
 
-            vertex(x * boxWidth, (y+1) * boxWidth, h1);
-            vertex((x+1) * boxWidth, y * boxWidth, h2);
-            vertex((x+1) * boxWidth, (y+1) * boxWidth, getHeight(y+1, x+1));
+            vertex(x * boxWidth, y * boxWidth, heightTL);
+            vertex((x+1) * boxWidth, y * boxWidth, heightTR);
+
+            vertex(x * boxWidth, (y+1) * boxWidth, heightBL);
+            vertex((x+1) * boxWidth, y * boxWidth, heightTR);
             endShape();
         }
     }
 
     shiftHeightMapRows();
-    addFrameDuration(millis() - startTime);
 }
 
 function getHeight(y, x){
